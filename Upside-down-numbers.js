672b25f4f0223edacc5ac4f250c6df8eca05213a
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-02-21 15:29:24 -0800

    Update Upside-down-numbers.js

--- Upside-down-numbers.js
+++ Upside-down-numbers.js
@@ -13,3 +13,27 @@ They are 0, 1, 8.
 
 
 // Solution
+
+const upsideDownNumbers = {
+  "0": 0,
+  "1": 1,
+  "8": 8,
+  "6": 9,
+  "9": 6
+};
+
+function isUpsideDown(number) {
+  const result = +number.toString().split("").reverse().map(item => upsideDownNumbers[item]).join("");
+
+  return result === number;
+}
+
+function solve(x, y) {
+  let count = 0;
+  for (let i = x; i < y; i++) {
+    if(isUpsideDown(i)) {
+      count++;
+    }
+  }
+  return count;
+};
